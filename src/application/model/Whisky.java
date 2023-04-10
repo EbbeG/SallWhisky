2cd776c16801eb151c49e0178a1faeee6883e76f
EbbeG <ebbe.gronbaek@gmail.com>
2023-04-11 00:21:53 +0200

    add fad to væske

--- src/application/model/Whisky.java
+++ src/application/model/Whisky.java
@@ -1,6 +1,10 @@
 package application.model;
 
 public class Whisky {
+    int nr;
     WhiskyType type;
     Væske væske;
+    String beskrivelse;
+    double alkoholProcent;
+    double fortyndelseMængde;
 }
