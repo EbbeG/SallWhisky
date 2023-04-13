a13ca75a2efb9109ed31d914e59cacf0fcdf3ada
cbdo <christofferdodt@gmail.com>
2023-04-13 10:32:59 +0200

    A

--- src/setup/StorageInitializer.java
+++ src/setup/StorageInitializer.java
@@ -1,14 +1,17 @@
 package setup;
 
 import application.controller.Controller;
-import application.model.Fad;
-import application.model.Lager;
-import application.model.Reol;
+import application.model.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageInitializer {
     public static void main(String[] args) {
         initStorage();
     }
+
 
     /**
      * Initializes the storage with some objects.
@@ -26,9 +29,30 @@ public class StorageInitializer {
 
         Fad fad1 = controller.createFad(1, 100, "Spanien", "Bourbon");
         Fad fad2 = controller.createFad(2, 150, "Spanien", "Bourbon");
+        Fad fad3 = controller.createFad(3, 100, "England", "Sherry");
+        Fad fad4 = controller.createFad(4, 150, "England", "Sherry");
+
 
         controller.addFadToReol(reol1, 1, fad1);
-        controller.addFadToReol(reol2, 1, fad2);
+        controller.addFadToReol(reol2, 1, fad3);
+        controller.addFadToReol(reol3, 1, fad2);
+        controller.addFadToReol(reol4, 1, fad4);
+
+        Destillering destillering1 = controller.createDestillering(LocalDate.of(2020, 01, 1), LocalDate.of(2023, 01, 1),1, "Snævar", 100, 53, Kornsort.EVERGREEN);
+        Destillering destillering2 = controller.createDestillering(LocalDate.of(2019, 01, 1), LocalDate.of(2022, 01, 1),2, "Snævar", 150, 49, Kornsort.IRINA);
+
+        Map<Destillering, Double> map = new HashMap<>();
+        map.put(destillering1, 100.0);
+
+        Map<Destillering, Double> map2 = new HashMap<>();
+        map.put(destillering2, 150.0);
+        map.put(destillering1, 100.0);
+
+        controller.fyldPåFadFraDestillering(fad1, map , LocalDate.of(2019,02,3));
+        controller.fyldPåFadFraDestillering(fad2, map2 , LocalDate.of(2019,02,3));
+
+
+
 
 
         controller.saveStorage();
