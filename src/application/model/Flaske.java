7f317d205d1b6a88610770f9e0c08d22f147bc49
cbdo <christofferdodt@gmail.com>
2023-04-14 10:24:02 +0200

    Flaske

--- src/application/model/Flaske.java
+++ src/application/model/Flaske.java
@@ -3,4 +3,23 @@ package application.model;
 import java.io.Serializable;
 
 public class Flaske implements Serializable {
+    private int flaskeNr;
+    private int mængde;
+    private Whisky whisky;
+
+    public Flaske(int flaskeNr, int mængde) {
+        this.flaskeNr = flaskeNr;
+        this.mængde = mængde;
+    }
+    public void visHistorik(){
+
+    }
+
+    public int getFlaskeNr() {
+        return flaskeNr;
+    }
+    public int getMængde() {
+        return mængde;
+    }
+
 }
